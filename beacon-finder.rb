fcffc04c4d6d9f86b2ceacb857378c8d3dc2dabc
sgrastar24 <45493569+sgrastar24@users.noreply.github.com>
2019-03-23 01:03:44 +0900

    Add files via upload

---
+++ beacon-finder.rb
@@ -0,0 +1,10 @@
+cask 'beacon-finder' do
+  version '1.0'
+  sha256 'dcfc34f5bdf8438f939debf366c06d7ef88c18605540c76bed794821c092f702'
+
+  url 'http://www17.plala.or.jp/t-casa/apps/BeaconFinder/v1.0/BeaconFinder.zip'
+  name 'BeaconFinder'
+  homepage 'https://github.com/sgrastar24/BeaconFinder'
+
+  app 'BeaconFinder.app'
+end
